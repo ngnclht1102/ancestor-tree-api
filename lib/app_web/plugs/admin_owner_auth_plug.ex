bbd6f1a5634f1ea1c9e0a5a22ff8b0af2267d3b1
namnguyen <ngnclht@gmail.com>
2020-12-20 06:03:05 +0700

    Auto fetch family

--- lib/app_web/plugs/admin_owner_auth_plug.ex
+++ lib/app_web/plugs/admin_owner_auth_plug.ex
@@ -4,6 +4,7 @@ defmodule App.Plugs.AdminOwnerPlug do
   """
   import Plug.Conn
   import Phoenix.Controller, only: [json: 2]
+  import Ecto.Query, only: [from: 2]
 
   alias App.{Repo}
   alias App.Family.Family
@@ -15,33 +16,26 @@ defmodule App.Plugs.AdminOwnerPlug do
   def call(conn, _opt) do
     if not is_nil(conn.params) and not is_nil(conn.params) do
       %{current_admin: current_admin} = conn.assigns
-      family_id = Map.get(conn.params, "family_id")
-      # this should be person id
-      person_id = Map.get(conn.params, "id")
-
-      if family_id do
-        conn |> check_family(family_id, current_admin)
-      else
-        if family_id do
-          person = Person |> Repo.get(person_id)
-
-          if not is_nil(person) do
-            conn |> check_family(person.family_id, current_admin)
-          else
-            render_unauthorized(conn)
+      family = case Map.get(conn.params, "family_id") do
+        nil ->
+          case from(p in Family, where: p.owner_id == ^current_admin.id, limit: 1) |> Repo.one() do
+            nil -> nil
+            family -> family
           end
-        else
-          render_unauthorized(conn)
-        end
+        family_id -> Family |> Repo.get(family_id)
+      end
+
+      if family do
+        conn |> check_family(family, current_admin)
+      else
+        render_unauthorized(conn)
       end
     else
       render_unauthorized(conn)
     end
   end
 
-  def check_family(conn, family_id, current_admin) do
-    family = Family |> Repo.get(family_id)
-
+  def check_family(conn, family, current_admin) do
     if not is_nil(family) and family.owner_id == current_admin.id do
       conn |> assign(:current_family, family)
     else
