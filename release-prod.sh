284df16b85bffaaff64b3142388586f0084fa072
namnguyen <ngnclht@gmail.com>
2021-05-16 04:58:30 +0700

    Modify scripts

--- release-prod.sh
+++ release-prod.sh
@@ -1,4 +1,9 @@
 mix clean
 source ./.env
+rm -rf _build/prod/rel
+mkdir _build/prod/rel
+cd assets && yarn install && yarn run deploy
+mix phx.digest
+mix deps.get
 mix compile
 MIX_ENV=prod mix release
